09a070968012df8e985d51cfcdd1dd30928133b1
Bahi Habash <bahi.habash.edu@gmail.com>
2026-04-14 01:14:31 +0200

    MODIFY: extended CurrentUser decorator functionalities

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,11 +1,36 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import type { Request } from 'express';
 import type { AccessTokenPayload } from '../../modules/auth/constants/token-payload.type';
 import type { RequestWithUser } from '../interfaces/Request.interface';
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext): AccessTokenPayload => {
+  (
+    data: (keyof AccessTokenPayload)[] | keyof AccessTokenPayload | undefined,
+    ctx: ExecutionContext,
+  ):
+    | Partial<AccessTokenPayload>
+    | AccessTokenPayload[keyof AccessTokenPayload] => {
     const request: RequestWithUser = ctx.switchToHttp().getRequest();
-    return request.user;
+    const user = request.user;
+
+    // Safety fallback if the route is public and no user is attached
+    if (!user) {
+      return {};
+    }
+
+    if (typeof data === 'string') {
+      return user[data] || null;
+    }
+
+    if (Array.isArray(data)) {
+      return data.reduce(
+        (result, key) => {
+          result[key] = user[key];
+          return result ?? null;
+        },
+        {} as Record<string, any>,
+      );
+    }
+
+    return user;
   },
 );

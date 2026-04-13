e30b916c9958038e56b644c3fc811c15d9afb646
Bahi Habash <bahi.habash.edu@gmail.com>
2026-04-14 01:15:51 +0200

    DOC: documented the CurrentUser decorator for better readability

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -2,6 +2,18 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import type { AccessTokenPayload } from '../../modules/auth/constants/token-payload.type';
 import type { RequestWithUser } from '../interfaces/Request.interface';
 
+/**
+ * Get the current user from the request.
+ * @param data - The key of the user property to get or an array of keys or undefined to get the whole user.
+ * @example
+ * ```typescript
+ * getCurrentUser(@CurrentUser() user: AccessTokenPayload) {}
+ * getCurrentUserId(@CurrentUser('id') id: string) {}
+ * getCurrentUserIds(@CurrentUser(['sys_role', 'club_id']) {}
+ * ```
+ * @param ctx - The execution context.
+ * @returns The current user or the requested property or an empty object if the user is not found.
+ */
 export const CurrentUser = createParamDecorator(
   (
     data: (keyof AccessTokenPayload)[] | keyof AccessTokenPayload | undefined,

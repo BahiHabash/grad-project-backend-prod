6763c2b15a685f7ba948b1aa72dc10467af9f616
Bahi Habash <bahi.habash.edu@gmail.com>
2026-04-14 05:35:34 +0200

    chore: register new modules and configure system imports
    Note: Controllers and services are not completed and will be modified later.

--- src/modules/club/club.module.ts
+++ src/modules/club/club.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ClubService } from './club.service';
 import { ClubController } from './club.controller';
 import { Club } from './entities/club.entity';
+import { Claim } from './entities/claim.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [ClubController],
   providers: [ClubService],
-  imports: [TypeOrmModule.forFeature([Club])],
+  imports: [TypeOrmModule.forFeature([Club, Claim])],
 })
 export class ClubModule {}

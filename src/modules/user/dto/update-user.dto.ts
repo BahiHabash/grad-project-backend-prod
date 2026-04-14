c1da13c465494134f370862c2cdfad8f9a98e24a
Bahi Habash <bahi.habash.edu@gmail.com>
2026-04-14 05:35:26 +0200

    feat: implement user profile management and favorites logic (WIP)
    Note: Controllers and services are not completed and will be modified later.

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,4 +1,21 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString, IsUrl, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @ApiProperty({ description: 'First name', required: false })
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  first_name?: string;
+
+  @ApiProperty({ description: 'Last name', required: false })
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  last_name?: string;
+
+  @ApiProperty({ description: 'Profile image URL', required: false })
+  @IsOptional()
+  @IsUrl()
+  profile_image_url?: string;
+}
---
+++ src/modules/user/dto/user-profile-res.dto.ts
@@ -0,0 +1,85 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { AccountStatus } from '../../../common/enums/account-status.enum';
+import { SystemRole } from '../../../common/enums/system-role.enum';
+import { MemberRole } from '../../../common/enums/member-role.enum';
+import { ClubStatus } from '../../club/constants/club-status.enum';
+import { Expose, Type } from 'class-transformer';
+
+class ClubResDto {
+  @ApiProperty()
+  @Expose()
+  id: string;
+
+  @ApiProperty({ description: 'Sofa Score Club ID', nullable: true })
+  @Expose()
+  sofa_score_club_id: string | null;
+
+  @ApiProperty()
+  @Expose()
+  name: string;
+
+  @ApiProperty()
+  @Expose()
+  logo_url: string | null;
+
+  @ApiProperty({ enum: ClubStatus })
+  @Expose()
+  status: ClubStatus;
+}
+
+export class UserProfileResDto {
+  @Expose()
+  @ApiProperty()
+  id: string;
+
+  @Expose()
+  @ApiProperty()
+  email: string;
+
+  @Expose()
+  @ApiProperty()
+  username: string;
+
+  @Expose()
+  @ApiProperty()
+  first_name: string;
+
+  @Expose()
+  @ApiProperty({ nullable: true })
+  last_name: string | null;
+
+  @Expose()
+  @ApiProperty()
+  is_verified: boolean;
+
+  @Expose()
+  @ApiProperty({ enum: AccountStatus })
+  status: AccountStatus;
+
+  @Expose()
+  @ApiProperty()
+  get is_club_member(): boolean {
+    return this.club_id !== null;
+  }
+
+  @Expose()
+  @ApiProperty({ nullable: true })
+  club_id: string | null;
+
+  @Expose()
+  @ApiProperty({ enum: MemberRole, nullable: true })
+  member_role: MemberRole | null;
+
+  @Expose()
+  @ApiProperty({ enum: SystemRole })
+  system_role: SystemRole;
+
+  @Expose()
+  @ApiProperty({ nullable: true })
+  profile_image_url: string | null;
+
+  @Expose()
+  @Type(() => ClubResDto)
+  @ApiProperty({ type: ClubResDto, required: false })
+  club?: ClubResDto;
+}
